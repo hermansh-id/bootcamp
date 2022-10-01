7db35aba07f33e4185f69bc0aec6e1a82609c056
remiplayer <miperifan@gmail.com>
2022-10-01 11:33:35 +0700

    irfanpangwangun

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # BOOTCAMP
 
-1. Nama : ____
-2. NPM  : ____
+1. Nama : Muhammad Irfan Pangwangun
+2. NPM  : 217006506
+3. Nama : Zulza Laddera Aripin
+4. NPM  : 217006063
--- create-confirmation.php
+++ create-confirmation.php
@@ -1,25 +1,19 @@
-<?php
-// import file
-_____("db_config.php");
+<?php 
+require_once("db_config.php");
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
   <meta charset="UTF-8">
-  <_____>Delete record</_____>
-  <_____ rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  <title>Delete record</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 </head>
-
 <body>
   <div class="container">
     <div class="alert alert-success mt-3" role="alert">
-      The record has been created!
+    The record has been created!
     </div>
-    <!-- link ke index.php -->
-    <_____ href="_____" class="btn btn-primary">Back</_____>
   </div>
 </body>
-
 </html>
